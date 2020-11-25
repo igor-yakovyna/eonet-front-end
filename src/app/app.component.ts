3fb4964e407b19c21ccc9af1bcea82ba3c316497
igor-yakovyna <YIgor27@gmail.com>
2020-11-26 01:15:37 +0200

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = ' Earth Observatory Natural Event Tracker';
+}
